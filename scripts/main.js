cb24c7e7952d9357a83b12a3f274465583b8683d
Sarahqtw <Sarahqtw@hotmail.com>
2022-02-28 13:00:58 +0800

    Stable working version

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 class BoggleGame {
 	constructor() {
+		// initialises menu bar, timer, scorer, and boggle board of letters
 		this.menu = new Menu(this);
 		this.timer = new Timer(this);
 		this.boggleBoard = new BoggleBoard(this);
@@ -8,6 +9,7 @@ class BoggleGame {
 		this.boggleBoard.freeze();
 	}
 
+	// when game begins or restarts, disable the menu buttons, reset the timer and scores, reset the board
 	start = () => {
 		this.menu.disable();
 		this.timer.reset();
@@ -15,11 +17,13 @@ class BoggleGame {
 		this.scorer.reset();
 	};
 
+	// when game has ended (time is up), re-enable menu so that player can play again. disable board buttons
 	end = () => {
 		this.menu.enable();
 		this.boggleBoard.freeze();
 	};
 
+	//
 	evaluateWord = (word) => {
 		this.scorer.evaluateWord(word);
 	};
@@ -72,7 +76,7 @@ class Timer {
 		this.boggleGame = boggleGame;
 		this.maxSeconds = 30;
 		this.circle = document.querySelector('circle');
-		this.secondsLeftHTML = document.querySelector('#duration');
+		this.secondsLeftHTML = document.querySelector('#seconds-left');
 		this.perimeter = this.circle.getAttribute('r') * 2 * Math.PI;
 		this.circle.setAttribute('stroke-dasharray', this.perimeter);
 	}
@@ -134,6 +138,7 @@ class BoggleBoard {
 		this.boggleGridContainer = document.querySelector('#boggle-container');
 		for (let row_idx = 0; row_idx < this.DIAMETER; row_idx++) {
 			let row = document.createElement('div');
+			row.classList.add('row');
 			for (let col_idx = 0; col_idx < this.DIAMETER; col_idx++) {
 				let tile = this.tiles[row_idx * this.DIAMETER + col_idx];
 				row.append(tile.getHTMLButton());
@@ -166,20 +171,27 @@ class BoggleBoard {
 	};
 
 	getNeighbours = (tile) => {
+		// get the indices of the 8 surrounding tiles
 		let neighbourIndices = [
+			// gets the row above
 			tile.idx - this.DIAMETER - 1,
 			tile.idx - this.DIAMETER,
 			tile.idx - this.DIAMETER + 1,
+			// gets tiles on the left and right
 			tile.idx - 1,
 			tile.idx + 1,
+			// gets the row below
 			tile.idx + this.DIAMETER - 1,
 			tile.idx + this.DIAMETER,
 			tile.idx + this.DIAMETER + 1
 		];
-
+		// rows on which you expect the neightbouring tiles to be, relative to yourself
+		// three on the row before, two on the same row, three on the row after
 		const correctRowOffsets = [ -1, -1, -1, 0, 0, 1, 1, 1 ];
+		// get the row you are on, get the rows of everyone else. then check if the offsets are correct
 		const currentRow = Math.floor(tile.idx / this.DIAMETER);
 		const rowOffsets = neighbourIndices.map((idx) => Math.floor(idx / this.DIAMETER) - currentRow);
+
 		let validIndices = [];
 		for (let i = 0; i < rowOffsets.length; i++) {
 			if (rowOffsets[i] === correctRowOffsets[i]) {
@@ -189,14 +201,18 @@ class BoggleBoard {
 				}
 			}
 		}
+		// creates a set of the valid neighbouring tiles
 		return new Set(validIndices.map((idx) => this.tiles[idx]));
 	};
 
 	setBoardStateFromTile = (currentTile) => {
 		let neighbours = this.getNeighbours(currentTile);
+		// get everything except the current tile and the tile just before it
 		let tilesBefore = new Set(this.currentTilesHolder.currentTiles.slice(0, -2));
 		for (let tile of this.tiles) {
-			if ((neighbours.has(tile) || tile === currentTile) && !tilesBefore.has(tile)) {
+			let isTileOutOfUndoRange = tilesBefore.has(tile);
+			let isTileAroundCursor = neighbours.has(tile) || tile === currentTile;
+			if (isTileAroundCursor && !isTileOutOfUndoRange) {
 				tile.enable();
 			} else {
 				tile.disable();
@@ -250,24 +266,29 @@ class CurrentTilesHolder {
 		return this.currentTiles.length;
 	};
 
+	// returns a boolean — true if the first letter has been clicked
 	hasWordStarted = () => {
 		return this.numTiles() > 0;
 	};
 
+	// adds the tile to the currentTiles array and changes the text being displayed in the DOM
 	addTile = (tile) => {
 		this.currentTiles.push(tile);
 		this.currentLettersHolder.innerText += tile.getLetter();
 	};
 
+	// removes the letter from the DOM and the tile from the currentTile array
 	popTile = () => {
 		this.currentLettersHolder.innerText = this.currentLettersHolder.innerText.slice(0, -1);
 		return this.currentTiles.pop();
 	};
 
+	//get the tile before the tile that you just inserted
 	getTileBefore = () => {
 		return this.currentTiles.at(-2);
 	};
 
+	// gets the tile that you've just inserted
 	peek = () => {
 		return this.currentTiles.at(-1);
 	};
@@ -373,6 +394,7 @@ class Scorer {
 		this.scoredWords = new Set();
 		this.totalScore = 0;
 		this.totalScoreHTML.innerText = '0';
+		this.guessedWordsHTML.innerText = '';
 	};
 }
 
