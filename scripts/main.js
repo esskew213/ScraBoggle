a1c465d76bc94944b4dcb1c64c523a2ea866724b
Sarahqtw <Sarahqtw@hotmail.com>
2022-03-02 11:35:22 +0800

    added dictionary of scrabble letter scores

--- scripts/main.js
+++ scripts/main.js
@@ -129,7 +129,34 @@ class BoggleBoard {
 			'UWILRG',
 			'PACEMD'
 		];
-
+		this.SCRABBLESCORING = {
+			a: 1,
+			b: 4,
+			c: 4,
+			d: 2,
+			e: 1,
+			f: 4,
+			g: 3,
+			h: 3,
+			i: 1,
+			j: 10,
+			k: 5,
+			l: 2,
+			m: 4,
+			n: 2,
+			o: 1,
+			p: 4,
+			q: 10,
+			r: 1,
+			s: 1,
+			t: 1,
+			u: 2,
+			v: 5,
+			w: 4,
+			x: 8,
+			y: 3,
+			z: 10
+		};
 		for (let idx = 0; idx < this.DIAMETER ** 2; idx++) {
 			this.tiles.push(new Tile(idx, this));
 		}
